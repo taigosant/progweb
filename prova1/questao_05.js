e5364cb5bc8b68ca6ded3dee2f5eebda1cb47798
taigosant <tss@icomp.ufam.edu.br>
2019-05-22 09:26:32 -0400

    quinta questao

---
+++ prova1/questao_05.js
@@ -0,0 +1,38 @@
+class Venda {
+    constructor(id, quant, valor) {
+        this.id = id;
+        this.quant = quant;
+        this.valor = valor;
+    }
+
+    getId() {
+        return id;
+    }
+    setId(id) {
+        this.id = id;
+    }
+    getQuantidade(){
+        return this.quant;
+    }
+    setQuantidade(qt){
+        this.quant = qt;
+    }
+    getValor(){
+        return this.valor;
+    }
+    setValor(val){
+        this.valor = val;
+    }
+
+    getValorTotal(){
+        return this.valor * this.quant;
+    }
+}
+
+venda1 = new Venda("a1", 2, 6.5);
+venda1.setValor(3)
+venda2 = new Venda("a2", 5, 2.5);
+
+console.log("[VENDA 1 TOTAL]", venda1.getValorTotal());
+console.log("[VENDA 2 TOTAL]", venda2.getValorTotal());
+
